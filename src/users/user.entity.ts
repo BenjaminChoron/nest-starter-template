d9f953db016b8a4f031b371bd300ad446a5d4d4d
BenjCH <benjamin.choron@outlook.com>
2024-07-13 15:23:14 +0200

    🔇 Remove unnecessary logger

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,10 +1,7 @@
-import { Logger } from '@nestjs/common';
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
-  private readonly logger = new Logger(User.name);
-
   @PrimaryGeneratedColumn()
   id: number;
 
