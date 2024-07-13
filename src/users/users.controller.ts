b8e5c75d8f061d8b24ffc818215abff9673c8a4f
BenjCH <benjamin.choron@outlook.com>
2024-07-13 15:28:17 +0200

    🚧 Serialize users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,17 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private authService: AuthService) {}
 
   @Post('/signup')
   async createUser(@Body() body: CreateUserDto) {
     const user = await this.authService.signup(body.email, body.password);
-
     return user;
   }
 }
