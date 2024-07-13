c5dbbf52418f28888a675f897203068fc45bbf06
BenjCH <benjamin.choron@outlook.com>
2024-07-13 15:03:48 +0200

    🚧 UsersService create method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private repo: Repository<User>,
+  ) {}
+
+  create(email: string, password: string) {
+    const uuid = uuidv4();
+    const user = this.repo.create({ uuid, email, password });
+
+    return this.repo.save(user);
+  }
+}
