85a42889c73ad12094850a836beaf8ab994951f2
BenjCH <benjamin.choron@outlook.com>
2024-07-14 23:50:00 +0200

    🔧 Add Cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Nest Starter Template')
     .setDescription(
