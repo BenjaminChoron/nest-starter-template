d5ddc42fc9f79e550c75e57b36530ee2eea8dc8f
BenjCH <benjamin.choron@outlook.com>
2024-07-16 15:32:18 +0200

    🔧 Update data source to use dev and test databases

--- src/config/data.source.ts
+++ src/config/data.source.ts
@@ -1,15 +1,21 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 require('dotenv').config();
 
+const isTestEnv = process.env.NODE_ENV === 'test';
+
 export const dbdatasource: DataSourceOptions = {
   type: 'postgres',
   host: process.env.DATABASE_HOST,
-  port: parseInt(process.env.DATABASE_PORT, 10),
-  database: process.env.DATABASE_NAME,
+  port: isTestEnv
+    ? parseInt(process.env.TEST_DATABASE_PORT, 10)
+    : parseInt(process.env.DATABASE_PORT, 10),
+  database: isTestEnv
+    ? process.env.TEST_DATABASE_NAME
+    : process.env.DATABASE_NAME,
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   synchronize: false,
-  entities: ['dist/**/*entity.js'],
+  entities: ['dist/**/*.entity.js'],
   migrations: ['dist/migrations/*.js'],
   migrationsTableName: 'migrations',
 };
